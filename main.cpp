5c0652ef6a93fb6cc74e13e891512770ff35b078
stanley6002 <chang.836@osu.edu>
2015-02-02 15:00:51 -0600

    update original optical flow file

--- main.cpp
+++ main.cpp
@@ -28,15 +28,15 @@ int main (int argc, const char * argv[])
 {
     
     
-//    if (!(camCapture = cvCaptureFromCAM(CV_CAP_ANY))) 
-//    {
-//        cout << "Failed to capture from camera" << endl;
-////        // goto exitCameraOpenFailed;
-//    }
+    if (!(camCapture = cvCaptureFromCAM(CV_CAP_ANY))) 
+    {
+        cout << "Failed to capture from camera" << endl;
+        // goto exitCameraOpenFailed;
+    }
     
     
-    cvSetCaptureProperty(camCapture, CV_CAP_PROP_FRAME_WIDTH,  640); 
-    cvSetCaptureProperty(camCapture, CV_CAP_PROP_FRAME_HEIGHT, 480); 
+    cvSetCaptureProperty(camCapture, CV_CAP_PROP_FRAME_WIDTH,  320); 
+    cvSetCaptureProperty(camCapture, CV_CAP_PROP_FRAME_HEIGHT, 240); 
     
     cout << "Camera opened successfully" << endl;
     
@@ -51,10 +51,8 @@ int main (int argc, const char * argv[])
     IplImage *imgGrayA=0;
     IplImage *imgGrayB=0;
     
-    Img_width=  640;
-    Img_height= 480;
-    
-    int win_size=21;   
+    Img_width=  320;
+    Img_height= 240;
     
     imgA = cvCreateImage(cvSize(Img_width, Img_height), IPL_DEPTH_8U, 3);
     imgB = cvCreateImage(cvSize(Img_width, Img_height), IPL_DEPTH_8U, 3);
@@ -64,41 +62,43 @@ int main (int argc, const char * argv[])
     imgGrayB = cvCreateImage(cvSize(Img_width, Img_height), IPL_DEPTH_8U, 1); 
     
     
-    imgA = cvLoadImage("/P1_1.jpg", 3);
-    imgB = cvLoadImage("/P1_2.jpg", 3);
+    //imgA = cvLoadImage("/P1_1.jpg", 3);
+    //imgB = cvLoadImage("/P1_2.jpg", 3);
     
-    int grabFrameRet;
- //   do 
- //       if ((cameraFrame = cvQueryFrame(camCapture))) 
- //       {
- //           frame = skipNFrames(camCapture,10);
- //           frame = cvQueryFrame(camCapture);
+    //int grabFrameRet;
+    do 
+        if ((cameraFrame = cvQueryFrame(camCapture))) 
+        {
+            frame = skipNFrames(camCapture,8);
+            frame = cvQueryFrame(camCapture);
             
-            //Img_width= frame->width;
-            //Img_height= frame->height;
-            Img_width=  640;
-            Img_height= 480;
-//            if (_1stframe== true)
-//            {
-//                imgB=cvCloneImage(frame);           
-//            }
-//            if(_1stframe== false)
-//           {
-//                imgA= frame;
+            Img_width= frame->width;
+            Img_height= frame->height;
+            //Img_width=  640;
+            //Img_height= 480;
+            if (_1stframe== true)
+            {
+                imgB=cvCloneImage(frame);           
+            }
+            if(_1stframe== false)
+           {
+                imgA= frame;
                 
-//                imgC= cvCloneImage(imgB);
-//                imgB= cvCloneImage(frame);
+                imgC= cvCloneImage(imgB);
+                //imgB= cvCloneImage(frame);
                 
                 cvCvtColor(imgA,imgGrayA, CV_BGR2GRAY);  
-                cvCvtColor(imgB,imgGrayB, CV_BGR2GRAY);  
+                cvCvtColor(imgC,imgGrayB, CV_BGR2GRAY);  
                 
                 
                 cv::Mat flow_mat;
                 vector<Point2f> corners,nextPts; vector<uchar> status; vector<float> err;
-                goodFeaturesToTrack(imgGrayA, corners, 500, 0.001,10);
-                cornerSubPix(imgGrayA, corners, Size(17,17), Size(-1,-1), TermCriteria( CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 40, 0.001 ));
+                goodFeaturesToTrack(imgGrayA, corners, 300, 0.001,10);
+                cornerSubPix(imgGrayA, corners, Size(11,11), Size(-1,-1), TermCriteria( CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 40, 0.001 ));
                 calcOpticalFlowPyrLK(imgGrayA, imgGrayB, corners, nextPts, status, err, Size(45,45));
-           
+              
+                imgB= cvCloneImage(frame);
+               
                  for ( int y = 0; y < (int) corners.size(); y++ ) 
                    {
                        int location_x= corners[y].x;
@@ -106,10 +106,10 @@ int main (int argc, const char * argv[])
                        float location_xB= nextPts[y].x;
                        float location_yB= nextPts[y].y;
                        
-                       CvScalar s= cvGet2D(imgA, location_y, location_x);
-                       //cvCircle(imgA, cvPoint(location_x, location_y), 1, CV_RGB(0, 255, 0), -1);
+                 //    CvScalar s= cvGet2D(imgA, location_y, location_x);
+                       cvCircle(imgA, cvPoint(location_x, location_y), 1, CV_RGB(0, 255, 0), -1);
                        cvCircle(imgB, cvPoint(location_xB, location_yB), 1, CV_RGB(0, 255, 0), -1);
-                       //cout<<location_x<<" "<<location_y<<" "<<location_xB<<" "<<location_yB<<" "<<s.val[0]<<" "<<s.val[1]<<" "<<s.val[2]<<endl;
+                //       cout<<location_x<<" "<<location_y<<" "<<location_xB<<" "<<location_yB<<" "<<endl;
                        cout<<location_x<<" "<<location_y<<" "<<location_xB<<" "<<location_yB<<" "<<0<<" "<<0<<" "<<0<<endl;
                    }
 //                 calcOpticalFlowFarneback( imgGrayA, imgGrayB, flow_mat, 0.1, 3, 15, 3, 5, 1.5,  cv::OPTFLOW_FARNEBACK_GAUSSIAN);                        
@@ -144,20 +144,21 @@ int main (int argc, const char * argv[])
 //                }
 //                
 //                
-//                flow_mat.release();
+               cout<<"second image"<<endl; 
+               flow_mat.release();
 //                
 //                // printf("[%d] - Sheta:%lf, Length:%lf\n",i , fVecSetha, fVecLength);     
 //                
                 cvShowImage("frame1",imgB);
 //                //cout<<"test 1"<<endl;
 //                    cvShowImage("frame2",imgC);
-//            }        
-//            _1stframe=false;   
+            }        
+            _1stframe=false;   
             
             
-//        }
+        }
     
-    //while (true) ;
+    while (true) ;
     cvReleaseImage(&frame);
     cvReleaseImage(&cameraFrame);
     cvReleaseImage(&imgA);
