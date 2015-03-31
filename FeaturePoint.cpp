b4b1b6771e4b50de671f2166634958abd35c18b2
stanley6002 <chang.836@osu.edu>
2015-03-31 17:41:11 -0500

    add feature track

---
+++ FeaturePoint.cpp
@@ -0,0 +1,9 @@
+//
+//  FeaturePoint.cpp
+//  Optical_flow 
+//
+//  Created by chih-hsiang chang on 3/27/15.
+//  Copyright 2015 __MyCompanyName__. All rights reserved.
+//
+
+#include "FeaturePoint.h"
---
+++ FeaturePoint.h
@@ -0,0 +1,7 @@
+//
+//  FeaturePoint.h
+//  Optical_flow 
+//
+//  Created by chih-hsiang chang on 3/27/15.
+//  Copyright 2015 __MyCompanyName__. All rights reserved.
+//
